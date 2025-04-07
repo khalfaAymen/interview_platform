c08945d4a003b0f4c0f5e682a9d5bd7be1540274
khalfaaymen <khalfaaymen141@gmail.com>
2025-04-07 13:03:03 +0100

    implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,12 +5,17 @@ import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -34,20 +39,56 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
-        console.log("sign up", values);
+        const { name, email, password } = values;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
         toast.success("Account created successfully");
         router.push("/sign-in");
       } else {
-        console.log("sign in", values);
+        const { email, password } = values;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if (!idToken) {
+          toast.error("Error signing in");
+          return;
+        }
+        await signIn({
+          email,
+          idToken,
+        });
         toast.success("Logged in successfully");
         router.push("/");
       }
     } catch (error) {
       console.log("Error signing in", error);
-      toast.error(`There wass an error : ${error}`);
+      toast.error(`There was an error : ${error}`);
     }
     console.log(values);
   }
---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,122 @@
+"use server";
+
+import { auth, db } from "@/firebase/admin";
+import { FirebaseError } from "firebase/app";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7; // 7 days
+
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
+
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: "User already exists",
+      };
+    }
+
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
+
+    return {
+      success: true,
+      message: "User created successfully",
+    };
+  } catch (error) {
+    console.error("Error signing up:", error);
+
+    if (error instanceof FirebaseError) {
+      if (error.code === "auth/email-already-in-use") {
+        return {
+          success: false,
+          message: "Email already in use",
+        };
+      }
+    }
+
+    return {
+      success: false,
+      message: "Error signing up",
+    };
+  }
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User not found",
+      };
+    }
+
+    // Set session cookie
+    await setSessionCookie(idToken);
+
+    return {
+      success: true,
+      message: "User signed in successfully",
+    };
+  } catch (error) {
+    console.error("Error signing in:", error);
+    return {
+      success: false,
+      message: "Error signing in",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000,
+  });
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK * 1000,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+
+  const sessionCookie = cookieStore.get("session")?.value || null;
+  if (!sessionCookie) {
+    return null;
+  }
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) {
+      return null;
+    }
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.error("Error getting current user:", error);
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+    const user = await getCurrentUser();
+    return !!user; // Return true if user is authenticated, false otherwise
+}
